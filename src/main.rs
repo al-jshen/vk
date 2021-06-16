4560023e3144840d33c5cfc0a177a11095535e03
Jeff Shen <jshen2014@hotmail.com>
2021-06-16 02:36:42 -0400

    make window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
 use ash::version::EntryV1_0;
 use ash::version::InstanceV1_0;
+use winit::window::Window;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let entry = unsafe { ash::Entry::new()? };
-    let instance = unsafe { entry.create_instance(&Default::default(), None)? };
-    unsafe { instance.destroy_instance(None) };
-    Ok(())
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
+struct VkApp {}
+
+impl VkApp {
+    fn init_window(event_loop: &EventLoop<()>) -> Window {
+        WindowBuilder::new()
+            .with_inner_size(winit::dpi::LogicalSize::new(WIDTH, HEIGHT))
+            .with_title("Vulkan")
+            .build(&event_loop)
+            .unwrap()
+    }
+
+    fn main_loop(event_loop: EventLoop<()>) {
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            }
+        })
+    }
+}
+
+fn main() {
+    let el = EventLoop::new();
+    let _win = VkApp::init_window(&el);
+    VkApp::main_loop(el);
 }
